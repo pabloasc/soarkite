b86eed65b6be85133af79c229c9fe7506e0c0263
pabloasc <pabloasc@gmail.com>
2024-11-20 15:43:02 +0100

    Leave one fivicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,9 +89,7 @@ export default function RootLayout({
         <link rel="canonical" href="https://soarkite.com" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icons/favicon-32x32.png" sizes="32x32" type="image/png" />
-        <link rel="icon" href="/icons/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/icons/favicon.ico" />
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
