8f24104209c783af76163120cd3ba0dfa648965d
Pablo Villalon <pabloasc@gmail.com>
2025-03-09 23:08:30 +0100

    Home page without login

--- src/lib/auth/server/supabase.ts
+++ src/lib/auth/server/supabase.ts
@@ -6,7 +6,7 @@ export async function getUserInfo() {
     const supabase = await createClient()
     const { data, error } = await supabase.auth.getUser()
     if (error || !data?.user) {
-      redirect('/auth/sign-in')
+      // redirect('/auth/sign-in')
     }
   
     return data.user;
